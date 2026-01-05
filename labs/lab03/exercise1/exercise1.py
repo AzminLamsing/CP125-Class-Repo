d200ccff2c40adb68dbe4e8422ef6dbb500aeca4
AzminLamsing <mohammadazminlamsing@gmail.com>
2026-01-05 13:07:34 +0800

    lab 3

--- labs/lab03/exercise1/exercise1.py
+++ labs/lab03/exercise1/exercise1.py
@@ -1,2 +1,6 @@
 def count_bright_spots(pixels):
-    pass
+    count = 0
+    for item in range (1,len(pixels)-1):
+      if (pixels[item] > pixels[item + 1] and pixels[item] > pixels[item - 1]):
+         count += 1
+    return count
--- labs/lab03/exercise2/exercise2.py
+++ labs/lab03/exercise2/exercise2.py
@@ -1,5 +1,16 @@
 def find_station(stations, name):
-    pass
+    for item in range (len(stations)):
+        if stations[item] == name :
+            return name 
+    return -1
 
+                    
 def count_stops(stations, start, stop):
-    pass
+    start_index = find_station(stations,start)
+    stop_index = find_station(stations,stop)
+     
+    if start_index == -1 or stop_index == -1:
+        return -1
+    else :
+        count = start_index - stop_index
+        return count
