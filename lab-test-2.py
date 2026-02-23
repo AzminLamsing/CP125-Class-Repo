748527d8e489d8b6b2f94fb78c895cf3ebcb07ff
AzminLamsing <mohammadazminlamsing@gmail.com>
2026-02-23 11:23:22 +0800

    up

---
+++ lab-test-2.py
@@ -0,0 +1,17 @@
+#Azmin Programmer 
+# i =0
+num = int(input("Enter your number : "))
+
+def enter_input(num) :
+    new_list = []
+    for i in range (5) :
+    new_list.append
+    return new_list
+
+
+
+num = int(input("Enter your number : "))
+result = enter_input(num)
+
+print(num)
+print(num_list)
