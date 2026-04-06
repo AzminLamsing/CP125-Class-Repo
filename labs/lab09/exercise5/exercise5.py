556ab4d31ab4f78fd487c5409ff3d026f0253df0
AzminLamsing <mohammadazminlamsing@gmail.com>
2026-04-06 13:02:31 +0800

    lab 9 senang

--- labs/lab09/exercise5/exercise5.py
+++ labs/lab09/exercise5/exercise5.py
@@ -2,4 +2,13 @@ import pandas as pd
 
 
 def high_performers(filename):
-    pass
+    df = pd.read_csv(filename)
+    high_subject_score = df.loc[df["Math","Science","English","Physics","Chemistry"] > 85 ]
+    name = set(high_subject_score["Name"])
+    return {
+        "count": len(name),
+        "names": name
+    }
+
+result = high_performers("CP125-Class-Repo/labs/lab09/data/students.csv")
+print(result)
