e1163e6ec7304230872262c963b63f8e993bda52
AzminLamsing <mohammadazminlamsing@gmail.com>
2026-04-06 12:42:10 +0800

    lab9

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -1,5 +1,24 @@
 import pandas as pd
 
-
 def compare_averages(filename):
-    pass
+    df = pd.read_csv(filename)
+    average = df[["math_scores","science_scores","english_scores"]].mean().round(1)
+
+    best_subject = df.max(average)
+    worst_subject = df.min(average)
+
+    return {
+        "Math": averages["Math"],
+        "Science": averages["Science"],
+        "English": averages["English"],
+        "best_subject": best_subject,
+        "worst_subject": worst_subject
+    }
+
+ 
+    
+compare_averages()
+
+
+
+
--- labs/lab09/exercise3/exercise3.py
+++ labs/lab09/exercise3/exercise3.py
@@ -3,4 +3,18 @@ import matplotlib.pyplot as plt
 
 
 def show_math_trend(filename):
-    pass
+    df = pd.read_csv(filename)
+    # 2. Extract Math column
+    math_scores = df["Math"]
+    # 3. Create line chart
+    plt.plot(df.index, math_scores)
+    plt.xlabel= ("Student Index")
+    plt.ylabel = ("Math Score")
+    plt.title = ("Math Score Trends")
+    plt.show()
+
+    return len(df)
+
+
+count = show_math_trend("CP125-Class-Repo/labs/lab09/data/students.csv")
+print(count)
